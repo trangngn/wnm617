88454de5ca34d5678cc849d271c419464d9a3eff
Aditya Patil <adityapatil@adityas-mbp.gateway.sonic.net>
2018-12-12 14:43:02 -0800

    test1

--- static/js/detail.js
+++ static/js/detail.js
@@ -8,16 +8,24 @@ $(function() {
 	}
 
 	var detail_html = `
-		<div class="row">
-			<div class="col-sm-6">
+		
+		<div class="row mt-5">
+			<div class="col-sm-3 col-md-4 offset-md-2">
 				<div class="book">
 					<img src="static/resources/home/book${book_detail.id}.png" alt="book" class="img-thumbnail" width="250" height="300">
 					<figure>
+
+					<button type="button" class="btn mt-4"><a href="pay.html">Purchase</a></button>
+
+					<div class="mt-3">
 						<audio controls src="static/resources/detail/audio.mp4">Your browser does not support the <code>audio</code> element.</audio>
+					</div>
+					
 				</figure>
 			</div>
 		</div>
-		<div class="col-sm-6">
+
+		<div class="col-sm-6 col-md-6">
 			<div class="description">
 				<h3> <p class="font-weight-bold">${book_detail.title}</p></h3>
 				<h4><p class="text-secondary">Truth, Lies, and Leadership</p></h4>
@@ -31,6 +39,9 @@ $(function() {
 				<ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star"></ion-icon><ion-icon name="star">
 				</div>
 			</div>
+
+
+
 		</div>
 	`;
 
