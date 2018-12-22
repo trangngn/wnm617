47788e7670fa59b78450c579c37201f3bf9594ea
Trang Nguyen <htng92@gmail.com>
2018-12-21 20:44:10 -0800

    fix hamburger

--- static/js/home.js
+++ static/js/home.js
@@ -1,32 +1,47 @@
 $(function() {
-	var innerBooksHtml = "";
+	var filtered_books = books;
+	displayBooks();
 
-	console.log(books);
+	$('#search').keyup(function() {
+		filtered_books = [];
+    var searchField = $('#search').val();
+    var myExp = new RegExp(searchField, "i");
+    for (var i = 0; i < books.length; i++) {
+    	var book = books[i];
+    	if ((books.title).search(myExp) != -1) {
+    		filtered_books.push(book);
+    	}
+    }             
+    displayBooks();
+  });
 
-	for (var i = 0; i < books.length; i++) {
-		var book = books[i];
-		var book_html = `
-				<div class="pl-2 pr-2" style="cursor:pointer;" onClick="window.location='detail.html?id=${book.id}';">
-					<div class="card" style="width: 8rem;">
-						<img class="card-img-top" src="static/resources/home/book${book.id}.jpg" alt="Card image cap">
-						<div class="audio-play text-center card-body p-1">
-							<p class="card-text"><i class="far fa-play-circle"></i> Sample</p>
+	function displayBooks() {
+		var innerBooksHtml = "";
+		for (var i = 0; i < filtered_books.length; i++) {
+			var book = filtered_books[i];
+			var book_html = `
+					<div class="pl-2 pr-2" style="cursor:pointer;" onClick="window.location='detail.html?id=${book.id}';">
+						<div class="card" style="width: 8rem;">
+							<img class="card-img-top" src="static/resources/home/book${book.id}.jpg" alt="Card image cap">
+							<div class="audio-play text-center card-body p-1">
+								<p class="card-text"><i class="far fa-play-circle"></i> Sample</p>
+							</div>
 						</div>
-					</div>
-					<div class="book-title">
-						<p>
-							Written by:<br>${book.authors[0].first_name} ${book.authors[0].last_name}<br>
-							<i class="text-warning fas fa-star"></i>
-							<i class="text-warning fas fa-star"></i>
-							<i class="text-warning fas fa-star"></i>
-							<i class="text-warning fas fa-star"></i>
-							<i class="text-warning fas fa-star-half-alt"></i>
-						</p>
-					</div>
-				</div> 
-		`;
-		innerBooksHtml += book_html;
-	}
+						<div class="book-title">
+							<p>
+								Written by:<br>${book.authors[0].first_name} ${book.authors[0].last_name}<br>
+								<i class="text-warning fas fa-star"></i>
+								<i class="text-warning fas fa-star"></i>
+								<i class="text-warning fas fa-star"></i>
+								<i class="text-warning fas fa-star"></i>
+								<i class="text-warning fas fa-star-half-alt"></i>
+							</p>
+						</div>
+					</div> 
+			`;
+			innerBooksHtml += book_html;
+		}
 
-	$('.books-container').html(innerBooksHtml);
+		$('.books-container').html(innerBooksHtml);
+	}
 });
